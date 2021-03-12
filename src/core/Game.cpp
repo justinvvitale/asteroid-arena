e5d34f46057d87f3232584e9f276d657ab63e126
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2021-03-12 15:27:25 +1100

    Enable culling and depth

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -31,15 +31,23 @@ void Game::start(int argc, char **argv) {
 void Game::init() {
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_FLAT);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+
 }
 
 // Callbacks
 
 void Game::reshape(int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(65.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glTranslatef(0.0, 0.0, -5.0);
@@ -47,7 +55,26 @@ void Game::reshape(int w, int h) {
 
 void Game::display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glDisable(GL_DEPTH_TEST);
+
+    glColor3f(1,1,1);
+
+    glPointSize(10);
+    glBegin(GL_LINE_LOOP);
+        glVertex3f(-0.5, -0.5, -0.5);
+        glVertex3f(0.5, -0.5, -0.5);
+        glVertex3f(0.5, 0.5, -0.5);
+        glVertex3f(-0.5, 0.5, -0.5);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(0,1,0);
+
+        glVertex3f(-0.25, -0.25, -0.75); // 1
+        glVertex3f(0.75, -0.25, -0.75); // 2
+        glVertex3f(0.75, 0.75, -0.75); // 3
+        glVertex3f(-0.25,0.75, -0.75); // 4
+    glEnd();
+
 
     int err;
     while ((err = glGetError()) != GL_NO_ERROR) {
