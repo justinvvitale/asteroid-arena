a7e939cfa204827499bd224165a5bf7ffb2e8609
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2021-04-21 22:42:30 +1000

    Simple player entity

--- src/A2/entities/PlayerEntity.h
+++ src/A2/entities/PlayerEntity.h
@@ -5,4 +5,50 @@
 #ifndef I3D_PLAYERENTITY_H
 #define I3D_PLAYERENTITY_H
 
+#include "../../core/Renderer.h"
+#include "../scripts/PlayerControllerScript.h"
+
+class PlayerEntity {
+
+public:
+
+    static Mesh getModel() {
+        Mesh mesh;
+
+        float playerSize = 30;
+        mesh.mode = GL_TRIANGLE_STRIP;
+        mesh.data = {
+                MeshData(-playerSize, playerSize, playerSize),     // Front top-left
+                MeshData(playerSize, playerSize, playerSize),      // Front top-right
+                MeshData(-playerSize, -playerSize, playerSize),    // Front bottom-left
+                MeshData(playerSize, -playerSize, playerSize),     // Front bottom-right
+                MeshData(playerSize, -playerSize, -playerSize),    // Back bottom-right
+                MeshData(playerSize, playerSize, playerSize),      // Front top-right
+                MeshData(playerSize, playerSize, -playerSize),     // Back top-right
+                MeshData(-playerSize, playerSize, playerSize),     // Front top-left
+                MeshData(-playerSize, playerSize, -playerSize),    // Back top-left
+                MeshData(-playerSize, -playerSize, playerSize),    // Front bottom-left
+                MeshData(-playerSize, -playerSize, -playerSize),   // Back bottom-left
+                MeshData(playerSize, -playerSize, -playerSize),    // Back bottom-right
+                MeshData(-playerSize, playerSize, -playerSize),    // Back top-left
+                MeshData(playerSize, playerSize, -playerSize)      // Back top-right
+        };
+
+        return mesh;
+    }
+
+    static Entity* getEntity() {
+        Entity* player = new Entity("player");
+
+        MeshComponent* meshRenderer = new MeshComponent();
+        meshRenderer->setMesh(getModel());
+        player->addComponent(meshRenderer);
+
+        PlayerControllerScript* controllerScript = new PlayerControllerScript();
+        player->addComponent(controllerScript);
+
+        return player;
+    }
+};
+
 #endif //I3D_PLAYERENTITY_H
